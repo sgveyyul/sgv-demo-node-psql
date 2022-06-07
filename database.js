e61e2907174bc2e5a5b284f2a0e5ec6f17e771cc
Yul Stewart Gurrea <Yul.Stewart.Gurrea@ph.ey.com>
2022-06-07 12:44:14 +0800

    initial commit simple user crud with pgsql

---
+++ database.js
@@ -0,0 +1,12 @@
+const { Client } = require('pg')
+require('dotenv').config()
+
+const client = new Client({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    port: process.env.DATABASE_PORT,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+})
+
+module.exports = client
