4247d80e60a99fa9a175ec1c52bf06047b411596
Yul Stewart Gurrea <Yul.Stewart.Gurrea@ph.ey.com>
2022-06-07 16:34:54 +0800

    updated database client

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const { Client } = require('pg')
 require('dotenv').config()
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 module.exports = client
