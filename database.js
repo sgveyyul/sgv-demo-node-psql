65566e0098ae7714238ef6c607b7408aea2f1a47
Yul Stewart Gurrea <Yul.Stewart.Gurrea@ph.ey.com>
2022-06-07 14:20:45 +0800

    updated node modules

--- database.js
+++ database.js
@@ -6,7 +6,10 @@ const client = new Client({
     user: process.env.DATABASE_USER,
     port: process.env.DATABASE_PORT,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
+    database: process.env.DATABASE_NAME,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 module.exports = client
